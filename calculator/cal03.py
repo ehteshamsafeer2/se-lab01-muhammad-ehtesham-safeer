10586b00001748730b8a8a7860e1e92312816cb0
msamiullah123 <msamimashfaqtahir@gmail.com>
2022-09-28 11:51:49 -0700

    Completed Successfully

---
+++ calculator/cal03.py
@@ -0,0 +1,14 @@
+# calculator in Python (v0.3) 
+
+def add(num1, num2):
+ return num1+num2 
+def sub(num1, num2):
+ return num1-num2 
+def mul(num1, num2): 
+return num1*num2 
+
+# main 
+
+print(add(5, 3)) 
+print(sub(5, 3)) 
+print(mul(5, 3))
